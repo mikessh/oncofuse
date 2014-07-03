4a11d94b67352a2b81070740d24cdb4aea4e39ad
mikesh <mikhail.shugay@gmail.com>
2014-07-04 01:11:32 +0400

    2 prev

--- src/main/groovy/es/unav/oncofuse/legacy/Oncofuse.groovy
+++ src/main/groovy/es/unav/oncofuse/legacy/Oncofuse.groovy
@@ -1,5 +1,5 @@
 /**
- Copyright 2013 Mikhail Shugay (mikhail.shugay@gmail.com)
+ Copyright 2013-14 Mikhail Shugay (mikhail.shugay@gmail.com)
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
