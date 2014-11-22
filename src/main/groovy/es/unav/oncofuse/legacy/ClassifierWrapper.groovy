5885df2775184bc899029cc3e8bea81185f1c512
mikesh <mikhail.shugay@gmail.com>
2014-11-22 12:40:52 +0300

    Some cleanup

--- src/main/groovy/es/unav/oncofuse/legacy/ClassifierWrapper.groovy
+++ src/main/groovy/es/unav/oncofuse/legacy/ClassifierWrapper.groovy
@@ -29,39 +29,6 @@ import weka.core.converters.ArffLoader
 class ClassifierWrapper {
     Instances trainingData
     SerializedClassifier sc
-    String schema = "@RELATION ONCOFUSE\n" +
-            "@ATTRIBUTE\t5_r_prom1\tNUMERIC\n" +
-            "@ATTRIBUTE\t5_r_prom2\tNUMERIC\n" +
-            "@ATTRIBUTE\tr_self_pii\tNUMERIC\n" +
-            "@ATTRIBUTE\tr_pii\tNUMERIC\n" +
-            "@ATTRIBUTE\tr_pii_expr\tNUMERIC\n" +
-            "@ATTRIBUTE\tr_CTF\tNUMERIC\n" +
-            "@ATTRIBUTE\tr_G\tNUMERIC\n" +
-            "@ATTRIBUTE\tr_H\tNUMERIC\n" +
-            "@ATTRIBUTE\tr_K\tNUMERIC\n" +
-            "@ATTRIBUTE\tr_P\tNUMERIC\n" +
-            "@ATTRIBUTE\tr_TF\tNUMERIC\n" +
-            "@ATTRIBUTE\t3_r_utr1\tNUMERIC\t\n" +
-            "@ATTRIBUTE\tb_CTF\tNUMERIC\n" +
-            "@ATTRIBUTE\tb_G\tNUMERIC\n" +
-            "@ATTRIBUTE\tb_H\tNUMERIC\n" +
-            "@ATTRIBUTE\tb_K\tNUMERIC\n" +
-            "@ATTRIBUTE\tb_P\tNUMERIC\n" +
-            "@ATTRIBUTE\tb_TF\tNUMERIC\t\n" +
-            "@ATTRIBUTE\t3_l_prom1\tNUMERIC\n" +
-            "@ATTRIBUTE\t3_l_prom2\tNUMERIC\n" +
-            "@ATTRIBUTE\tl_self_pii\tNUMERIC\n" +
-            "@ATTRIBUTE\tl_pii\tNUMERIC\n" +
-            "@ATTRIBUTE\tl_pii_expr\tNUMERIC\n" +
-            "@ATTRIBUTE\tl_CTF\tNUMERIC\n" +
-            "@ATTRIBUTE\tl_G\tNUMERIC\n" +
-            "@ATTRIBUTE\tl_H\tNUMERIC\n" +
-            "@ATTRIBUTE\tl_K\tNUMERIC\n" +
-            "@ATTRIBUTE\tl_P\tNUMERIC\n" +
-            "@ATTRIBUTE\tl_TF\tNUMERIC\n" +
-            "@ATTRIBUTE\t5_l_utr1\tNUMERIC\n" +
-            "@ATTRIBUTE\tclass\t{0,1}\n" +
-            "@DATA"
 
     ClassifierWrapper(String trainingSetFname, String model) {
         sc = new SerializedClassifier()
@@ -69,7 +36,6 @@ class ClassifierWrapper {
 
         ArffLoader loader = new ArffLoader()
         loader.setFile(new File(trainingSetFname))
-        //loader.setSource(new ByteArrayInputStream(schema.getBytes()))
         trainingData = loader.getDataSet()
         trainingData.setClassIndex(trainingData.numAttributes() - 1)
     }
