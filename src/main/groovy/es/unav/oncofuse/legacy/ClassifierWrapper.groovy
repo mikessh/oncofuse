7822fc26cf7cb549a7779e0434bd65f42ded02b2
mikesh <mikhail.shugay@gmail.com>
2014-11-22 12:25:37 +0300

    Pom fix, license in headers, add schema to resources

--- src/main/groovy/es/unav/oncofuse/legacy/ClassifierWrapper.groovy
+++ src/main/groovy/es/unav/oncofuse/legacy/ClassifierWrapper.groovy
@@ -1,4 +1,25 @@
+/*
+ * Copyright 2013-2014 Mikhail Shugay (mikhail.shugay@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Last modified on 22.11.2014 by mikesh
+ */
+
+
+
 package es.unav.oncofuse.legacy
+
 
 import weka.classifiers.misc.SerializedClassifier
 import weka.core.Instance
@@ -47,8 +68,8 @@ class ClassifierWrapper {
         sc.setModelFile(new File(model))
 
         ArffLoader loader = new ArffLoader()
-        //loader.setFile(new File(trainingSetFname))
-        loader.setSource(new ByteArrayInputStream(schema.getBytes()))
+        loader.setFile(new File(trainingSetFname))
+        //loader.setSource(new ByteArrayInputStream(schema.getBytes()))
         trainingData = loader.getDataSet()
         trainingData.setClassIndex(trainingData.numAttributes() - 1)
     }
