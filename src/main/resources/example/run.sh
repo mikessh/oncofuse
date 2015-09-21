036535f29502f49a7f6b279ca86eda784774f89b
mikesh <mikhail.shugay@gmail.com>
2015-09-21 04:09:23 +0300

    Added batch file to run all examples

---
+++ src/main/resources/example/run.sh
@@ -0,0 +1,7 @@
+ONCOFUSE="java -Xmx2G -jar ../Oncofuse.jar"
+$ONCOFUSE example_coord.txt coord - out.txt
+$ONCOFUSE example_fcatcher.txt fcatcher EPI out.txt
+$ONCOFUSE example_rnastar.txt rnastar EPI out.txt
+$ONCOFUSE example_starfusion.txt starfusion AVG out.txt
+$ONCOFUSE example_tophat_post.txt tophat-post EPI out.txt
+$ONCOFUSE example_tophat.txt tophat EPI out.txt
